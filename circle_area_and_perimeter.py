4261359a197a7dfe8c9d0b362b116d903373371f
Metodi Angelov <101995681+Ubersicka@users.noreply.github.com>
2022-03-21 00:01:21 +0200

    Create circle_area_and_perimeter.py

---
+++ circle_area_and_perimeter.py
@@ -0,0 +1,7 @@
+import math
+
+circle_r = float(input())
+circle_area = math.pi * circle_r ** 2
+print(f'{circle_area:.2f}')
+circle_perimeter = 2 * math.pi * circle_r
+print(f'{circle_perimeter:.2f}')
